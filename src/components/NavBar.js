7394d2930025c8ad9790e13f777e77794cb421d1
aakash25 <aakash.matkar21@vit.edu>
2024-09-12 20:27:29 +0530

    Adding src.components

---
+++ src/components/NavBar.js
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import LandingPage from "./LandingPage";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import { updateUsername } from "../store/store";
+import {
+  BrowserRouter as Router , Routes, Route
+} from "react-router-dom";
+
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const GITHUB_CLIENT_ID = "ac13de5979caa668c1f2";
+const gitHubRedirectURL = "http://alhussain-shaikh.github.io/CodeCompass/api/auth/github";
+const path = "/";
+
+
+
+export const NavBar = () => {
+
+  const dispatch = useDispatch();
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+  const [user, setUser] = useState();
+
+  const username = useSelector((state) => state.user.username);
+  console.log("username is : ",username);
+
+  const storeUpdate = (e) =>{
+    dispatch(updateUsername(e.login))
+  }
+
+  useEffect(() => {
+    (async function () {
+      const usr = await axios
+        .get(`http://alhussain-shaikh.github.io/api/me`, {
+          withCredentials: true,
+        })
+        .then((res) => res.data);
+        storeUpdate(usr);
+      setUser(usr);
+    })();
+   
+  }, []);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  const logoutUser = () => {
+    dispatch(updateUsername(""))
+    setUser("")
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            {/* <img src={logo} alt="Logo" /> */}
+            <h1 style={{color:"white"}}>CodeCompass</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+         
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link> */}
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Services</Nav.Link>
+              <Nav.Link href="#analysis" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('analysis')} >Analysis</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>
+
+              {!user ? (
+                <a href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}>
+                  <button className="vvd"><span>Login With GitHub</span></button>
+                </a>
+              ) : (
+                <>
+                <button className="vvd"><span>Welcome {user.login}</span></button>
+                <button onClick={logoutUser}><LogoutIcon color="#fff" /></button>
+                </>
+              )}
+              {/* <a href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}>
+                <button className="vvd"><span>Login With GitHub</span></button>
+              </a> */}
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
---
+++ src/components/profile/issueRecommendation.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './issueRecommendation.css';
+import { useSelector } from 'react-redux';
+
+function GitHubIssueFetcher() {
+  const [issueTypes, setIssueTypes] = useState('');
+  const [numIssues, setNumIssues] = useState(0);
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [toggle, setToggle] = useState(false);
+  const githubToken = useSelector((state)=>state.github.github_token)
+  
+  const fetchGitHubIssues = async () => {
+    try {
+      setLoading(true);
+
+      const labels = issueTypes.split(',').map(issueType => `label:${issueType.trim()}`).join('+');
+      const apiUrl = `https://api.github.com/search/issues?q=state:open+is:issue+${labels}`;
+      const headers = {
+        Authorization: `token ${githubToken}`,
+      };
+      const response = await axios.get(apiUrl, { headers });
+
+      const data = response.data;
+
+      if ('items' in data) {
+        const issues = data.items.slice(0, numIssues).map((item, index) => {
+          const ownerName = item.repository_url.split('/')[4];
+          const repoName = item.repository_url.split('/')[5];
+
+          return (
+            
+              <div key={index} className='issueOutput'>
+                <div className='cardHeader'>
+                  <img className='profilePic' src={item.user.avatar_url} alt="" srcset="" />
+                  <a className='RepositoryURL' href={item.html_url} target='_blank'>Repo: {repoName}</a>
+                  <span className='owner'>Owner: {ownerName}</span>
+
+                </div>
+                <div className='issueDescription'>
+                  <p className='title'><b>Title: </b>{item.title}</p>
+                  <p >{item.body}</p>
+                </div>
+              </div>
+        
+          );
+        });
+
+        setResult(issues);
+      } else {
+        setResult(['No issues found for the given type.']);
+      }
+
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="card">
+      <h1>Tag Based Issue Reccomendation</h1>
+      <div className='inputLabel'>
+        Enter the types of issues you want to work on (e.g., bug, enhancement):
+        <input
+          type="text"
+          value={issueTypes}
+          onChange={(e) => setIssueTypes(e.target.value)}
+        />
+      </div>
+      <div className='numberOfIssues'>
+        Enter the number of issues you want to see:
+        <input
+          type="number"
+          value={numIssues}
+          onChange={(e) => setNumIssues(e.target.value)}
+        />
+      </div>
+      <button className='fetch' onClick={fetchGitHubIssues}>Fetch Issues</button>
+     
+        {loading ? <p className="loading">Loading...</p> :  <div className='outputContainer'> {result} </div>}
+      
+    </div>
+  );
+}
+export default GitHubIssueFetcher;
